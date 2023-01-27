dc21c1e026f7f3d1da3ee63b48fc19b90b279008
Евгений <msk9199625687@yandex.ru>
2023-01-27 14:36:10 +0300

    production

--- src/index.js
+++ src/index.js
@@ -5,11 +5,39 @@ const inputName = document.querySelector('.order__input--name')
 const images = document.querySelectorAll('.products__image')
 
 
+const showMessage = (text) => {
+    const message = document.createElement('P')
+    message.style.fontSize = '32px'
+    message.style.position = 'fixed'
+    message.style.bottom = '0'
+    message.style.right = '0'
+    message.style.left = '0'
+    message.style.padding = '100px'
+    message.style.color = '#ffffff'
+    message.style.backgroundColor = 'green'
+    message.textContent = text
+    document.querySelector('.order').appendChild(message)
+    setTimeout(() => message.remove(), 2000)
+}
+
 //обработчик событий формы
 formOrder.addEventListener('submit', (evt) => {
     evt.preventDefault();
-    const formData = new FormData(evt.target)
-    console.log(formData)
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+        title: `${document.querySelector('.order__input--name').value}`,
+        body: `${document.querySelector('.order__input--address').value}`,
+        userId: `${document.querySelector('.order__input--number').value}`,
+      }),
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+    },
+    })
+    .then((response) => response.json())
+    .then((json) => console.log(json))
+    .then(() => showMessage('Данные успешно отправлены, ответ в консоле'))
+    .catch((err) => showMessage(err))
 })
 
 //обработчик события инпута с именем
@@ -18,28 +46,42 @@ inputName.addEventListener('keydown', (evt) => {
         evt.preventDefault()
     }
 })
-const arrImages = [...images]
-for (let i = 0; i < arrImages; i++) {
-    arrImages[i].addEventListener('click', () => {
-        console.log('here')
-        const template = document.querySelector('.template-popup')
-            .content
-            .querySelector('.popup')
-        const popup = template.cloneNode(true)
-        popup.querySelector('.popup__image').src = arrImages[i].src
-        console.log(arrImages[i].src)
-        document.querySelector('body').appendChild(popup)
+
+//обработчик событий закрытия popup
+const popupHandler = () => {
+    const popup = document.querySelector('.popup')
+    popup.addEventListener('click', (evt) => {
+        if (!evt.target.classList.contains('popup__image')) {
+            popup.remove()
+        }
     })
 }
-console.log(arrImages)
 
-images.forEach((image) => {
-    image.addEventListener('click', (evt) => {
-        const template = document.querySelector('.template-popup')
-            .content
-            .querySelector('.popup')
-        const popup = template.cloneNode(true)
-        popup.querySelector('.popup__image').src = evt.target.src
-        document.querySelector('body').appendChild(popup)
-    })
-})
+//открытие popup фотографий и клик на фотографии
+const openPopupImage = () => {
+    const arrImages = []
+
+    for(let i = 0; i < images.length; i++) {
+        arrImages.push(images[i].src)
+        images[i].addEventListener('click', () => {
+            const template = document.querySelector('.template-popup')
+                .content
+                .querySelector('.popup')
+            const popup = template.cloneNode(true)
+            popup.querySelector('.popup__image').src = images[i].src
+            popup.querySelector('.popup__image').addEventListener('click', (evt) => {
+                const index = arrImages.indexOf(evt.target.src)
+                if (index + 1 == images.length) {
+                    popup.querySelector('.popup__image').src = images[0].src
+                } else {
+                    popup.querySelector('.popup__image').src = images[index + 1].src
+                }
+            })
+            document.querySelector('body').appendChild(popup)
+            popupHandler()
+        })
+    }
+    
+}
+
+openPopupImage()
